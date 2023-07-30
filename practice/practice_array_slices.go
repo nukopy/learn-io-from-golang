80e7e610b8b226233971e1b4cd9d18c6bdcc140b
nukopy <nukopy@gmail.com>
2023-07-30 12:09:22 +0900

    chore: add example about basics of array and slices

---
+++ practice/practice_array_slices.go
@@ -0,0 +1,21 @@
+package practice
+
+import (
+	"fmt"
+)
+
+func PracticeArraySlices() {
+	fmt.Println("----- Practice Array Slices -----")
+
+	// array
+	const len int = 10 // len は const でないといけない
+	var arr [len]int // 配列はゼロ値で初期化される
+	fmt.Println(arr)
+
+	arr2 := [len]int{1, 2, 3, 4, 5}
+	fmt.Println(arr2)
+
+	// var lenVar int = 10
+	// var arrVar [lenVar]int --> コンパイルエラーになる: invalid array length lenVar
+	// fmt.Println(arrVar)
+}
---
+++ practice/practice_slices_len_cap.go
@@ -0,0 +1,66 @@
+package practice
+
+import "fmt"
+
+func PracticeSlicesLenCap() {
+	sliceBasic()
+	sliceWithMake()
+}
+
+func sliceBasic() {
+	s := []int{2, 3, 5, 7, 11, 13}
+	printSlice(s)
+
+	// Slice the slice to give it zero length.
+	s = s[:0]
+	printSlice(s)
+
+	// Extend its length.
+	s = s[:4]
+	printSlice(s)
+
+	// Drop its first two values.
+	s = s[2:]
+	printSlice(s)
+
+	// Access idx = 0, 1
+	s = s[:2]
+	printSlice(s)
+}
+
+func printSlice(s []int) {
+	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
+}
+
+/* 出力
+len=6 cap=6 [2 3 5 7 11 13]
+len=0 cap=6 []
+len=4 cap=6 [2 3 5 7]
+len=2 cap=4 [5 7]
+len=2 cap=4 [5 7]
+*/
+
+func sliceWithMake() {
+	a := make([]int, 5)
+	printSlice2("a", a)
+
+	b := make([]int, 0, 5) // make([]T, length, capacity)
+	printSlice2("b", b)
+
+	c := b[:2]
+	printSlice2("c", c)
+
+	d := c[2:5]
+	printSlice2("d", d)
+}
+
+func printSlice2(s string, x []int) {
+	fmt.Printf("%s len=%d cap=%d %v\n", s, len(x), cap(x), x)
+}
+
+/* 出力
+a len=5 cap=5 [0 0 0 0 0]
+b len=0 cap=5 []
+c len=2 cap=5 [0 0]
+d len=3 cap=3 [0 0 0]
+*/
